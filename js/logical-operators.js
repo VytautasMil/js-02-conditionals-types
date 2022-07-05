7fd9fab5141dc9bdd8d17348432095d22ce7cfeb
Vytautas <vyckelis@gmail.com>
2022-07-05 14:45:19 +0300

    logical

---
+++ js/logical-operators.js
@@ -0,0 +1,9 @@
+// Sudetines salygos
+
+// loginis AND
+console.log('true && true ===', true && true);
+console.log('true && false ===', true && false);
+console.log('false && true ===', false && true);
+console.log('false && false ===', false && false);
+
+// loginis OR
